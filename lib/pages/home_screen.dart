5e90b0929eff1becd349608766f44b862095c4c8
Dulkith Bataduwa <dulkith@outlook.com>
2019-03-16 14:42:55 +0530

    Empty Home
    Add camera icon.

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:thuru_care_client/utils/thuru_care.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => new _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  AnimationController animCtrl;
+  Animation<double> animation;
+
+  AnimationController animCtrl2;
+  Animation<double> animation2;
+
+  bool showFirst = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Animation init
+    animCtrl = new AnimationController(
+        duration: new Duration(milliseconds: 500), vsync: this);
+    animation = new CurvedAnimation(parent: animCtrl, curve: Curves.easeOut);
+    animation.addListener(() {
+      this.setState(() {});
+    });
+    animation.addStatusListener((AnimationStatus status) {});
+
+    animCtrl2 = new AnimationController(
+        duration: new Duration(milliseconds: 1000), vsync: this);
+    animation2 = new CurvedAnimation(parent: animCtrl2, curve: Curves.easeOut);
+    animation2.addListener(() {
+      this.setState(() {});
+    });
+    animation2.addStatusListener((AnimationStatus status) {});
+  }
+
+  @override
+  void dispose() {
+    animCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(ThuruCare.name),
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          Padding(
+            child: Icon(Icons.search),
+            padding: const EdgeInsets.only(right: 10.0),
+          )
+        ],
+      ),
+      drawer: Drawer(),
+      body: new Center(
+          child: new Stack(
+        children: <Widget>[],
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black87,
+        onPressed: () => {},
+        child: Icon(FontAwesomeIcons.camera, color: Colors.white),
+      ),
+    );
+  }
+}
+
+class CardView extends StatelessWidget {
+  final double cardSize;
+  CardView(this.cardSize);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+        child: new SizedBox.fromSize(
+      size: new Size(cardSize, cardSize),
+    ));
+  }
+}
