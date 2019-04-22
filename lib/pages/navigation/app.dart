7ef91e964882639c91eeb518a1831fadec40392f
Dulkith Bataduwa <dulkith@outlook.com>
2019-04-23 02:04:24 +0530

    Remove camera parameter

--- lib/pages/navigation/app.dart
+++ lib/pages/navigation/app.dart
@@ -7,6 +7,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomeScreen(cameras));
+        home: HomeScreen());
   }
 }
